d4ddf49db90808859a2d7a9d41bc3144573526ff
shradaya <shradaya@hotmail.com>
2022-08-12 10:32:52 +0545

    amend typo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ URL= 'https://api.github.com/repos/shradaya/GA-2/dispatches'
 Headers= {
     'Authorization': f"Bearer {os.environ['REPOSITORY_TOKEN']}",
     'Accept': 'application/vnd.github.v3+json',
-    'Content'-Type: 'application/json',
+    'Content-Type': 'application/json',
 }
 Body= {
     'event_type': 'custom_event_type'
